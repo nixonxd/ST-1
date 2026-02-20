c5eba0e3f16695ccfc6771618b8dd1afa3e5d2a7
Ermakov Alexei <lioshaermakov32@gmail.com>
2026-02-20 16:08:59 +0300

    Ermakov

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,24 +1,57 @@
 // Copyright 2025 UNN-CS
-#include <cstdint>
 #include "alg.h"
-
+#include <cstdint>
+#include <math.h>
 
 bool checkPrime(uint64_t value) {
-  // вставьте код функции
+  if (value <= 1)
+    return false;
+  if (value == 2)
+    return true;
+  if (value % 2 == 0)
+    return false;
+
+  uint64_t limit = (uint64_t)sqrt(value);
+  for (uint64_t i = 3; i <= limit; i += 2) {
+    if (value % i == 0)
+      return false;
+  }
   return true;
 }
 
 uint64_t nPrime(uint64_t n) {
-  // вставьте код функции
-  return 2;
+  if (n == 0)
+    return 0;
+
+  uint64_t count = 0;
+  uint64_t number = 1;
+
+  while (count < n) {
+    ++number;
+    if (checkPrime(number))
+      ++count;
+  }
+
+  return number;
 }
 
 uint64_t nextPrime(uint64_t value) {
-  // вставьте код функции
-  return 2;
+  uint64_t res = value + 1;
+
+  while (true) {
+    if (checkPrime(res))
+      return res;
+    ++res;
+  }
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-  // вставьте код функции
-  return 2;
+  uint64_t sum = 0;
+
+  for (uint64_t i = 2; i < hbound; ++i) {
+    if (checkPrime(i))
+      sum += i;
+  }
+
+  return sum;
 }
